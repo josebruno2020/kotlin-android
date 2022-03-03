edfe791b94465e5cbe557e0628e53e116ac70fa7
José Bruno <josebrunocampanholi@gmail.com>
2022-03-02 22:27:40 -0300

    Intro a recyclerView e adapter.

---
+++ app/src/main/java/jb/santos/orgs/ui/recycler/adapter/ListaProdutosAdapter.kt
@@ -0,0 +1,19 @@
+package jb.santos.orgs.ui.recycler.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import jb.santos.orgs.model.Produto
+
+class ListaProdutosAdapter(
+    private val produtos: List<Produto>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = produtos.size
+}
